aba892fd57d3998a82706cdde27940f3e3d823ab
MartinaPerozzi <perozzimartina@gmail.com>
2023-05-11 12:41:00 +0200

    + added seeder dish

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([]);
+        $this->call([
+            DishSeeder::class,
+        ]);
     }
 }
--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Dish;
 
 class DishSeeder extends Seeder
 {
@@ -14,6 +15,76 @@ class DishSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $dish = new Dish;
+        $dish->name = 'Big Mc Menu';
+        $dish->description = 'Il Grande Classico di McDonald’s è pronto a stupirti con il suo gusto ancora più irresistibile. Lasciati avvolgere dal pane più caldo, trasportare dal sapore della sua carne più succosa e goditi un’ulteriore aggiunta della sua inconfondibile salsa: lo scoprirai ancora più buono. Con patatine e Coca-Cola.';
+        $dish->price = '9.90';
+        $dish->visibility = 1;
+        $dish->image = 'https://picsum.photos/200';
+        $dish->save();
+
+        $dish = new Dish;
+        $dish->name = 'Chicken Salad';
+        $dish->description = 'Gli opposti si attraggono! Prova il mix perfetto tra la panatura del petto di pollo 100% italiano e la tenera insalata, insieme alla dolcezza dei pomodorini ciliegia e al gusto inconfondibile del Parmigiano Reggiano per un risultato dal sapore unico.';
+        $dish->price = '10.90';
+        $dish->visibility = 1;
+        $dish->image = 'https://picsum.photos/200';
+        $dish->save();
+
+        $dish = new Dish;
+        $dish->name = 'Filet-O-Fish';
+        $dish->description = 'A volte per sorprendere bastano tre semplici ingredienti: merluzzo impanato, formaggio, salsa tartara. Il risultato? Un Grande Classico che unisce il sapore del mare al gusto unico di McDonald’s.';
+        $dish->price = '5.40';
+        $dish->visibility = 1;
+        $dish->image = 'https://picsum.photos/200';
+        $dish->save();
+
+        $dish = new Dish;
+        $dish->name = 'CheeseBurger Menu';
+        $dish->description = 'Ci sono due tipi di persone: chi ama il cheeseburger e chi ama il cheeseburger. Tutti lo scelgono per il suo gusto semplice e irresistibile, per la carne 100% bovina da allevamenti italiani, il formaggio filante, la cipolla a dadini, il ketchup e la senape.';
+        $dish->price = '6.90';
+        $dish->visibility = 1;
+        $dish->image = 'https://picsum.photos/200';
+        $dish->save();
+
+        $dish = new Dish;
+        $dish->name = 'Happy Meal';
+        $dish->description = 'Happy Meal, la nostra offerta per i bambini, si può comporre scegliendo tra 4 prodotti principali, 2 contorni, 9 bevande e 4 dessert. E inoltre puoi scegliere tra tante sorprese sempre diverse oppure un libro, per avvicinare i più piccoli al fantastico mondo della lettura.';
+        $dish->price = '5.90';
+        $dish->visibility = 1;
+        $dish->image = 'https://picsum.photos/200';
+        $dish->save();
+
+        $dish = new Dish;
+        $dish->name = 'Mc Flurry';
+        $dish->description = 'C’è chi non vede l’ora di chiudere il menu con un McFlurry®, un Sundae, con un Cono o con un Milkshake. C’è chi invece sceglie di gustarli ogni momento della giornata, per una pausa di pura dolcezza.';
+        $dish->price = '3.50';
+        $dish->visibility = 1;
+        $dish->image = 'https://picsum.photos/200';
+        $dish->save();
+
+        $dish = new Dish;
+        $dish->name = 'Crispy Mc Wrap';
+        $dish->description = 'Carne 100% bovina da allevamenti italiani, croccante bacon 100% da pancetta italiana, formaggio e l’inconfondibile salsa, avvolti in una gustosa tortilla.';
+        $dish->price = '10.70';
+        $dish->visibility = 1;
+        $dish->image = 'https://picsum.photos/200';
+        $dish->save();
+
+        $dish = new Dish;
+        $dish->name = 'Chicken Mc Wrap';
+        $dish->description = 'Carne 100% pollo da allevamenti italiani, croccante bacon 100% da pancetta italiana, formaggio e l’inconfondibile salsa, avvolti in una gustosa tortilla.';
+        $dish->price = '9.50';
+        $dish->visibility = 1;
+        $dish->image = 'https://picsum.photos/200';
+        $dish->save();
+
+        $dish = new Dish;
+        $dish->name = 'Gran Crispy Mc Bacon';
+        $dish->description = "Chi ama il Crispy McBacon® ne prenderebbe volentieri un altro e un altro e un altro e un altro ancora. Per questo c’è il Gran Crispy McBacon®: carne 100% bovina da allevamenti italiani, croccante bacon 100% da pancetta italiana, formaggio e l'inconfondibile salsa Crispy. Come il classico, ma ancora più grande.";
+        $dish->price = '8.50';
+        $dish->visibility = 1;
+        $dish->image = 'https://picsum.photos/200';
+        $dish->save();
     }
 }
