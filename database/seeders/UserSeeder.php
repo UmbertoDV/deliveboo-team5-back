ec0e50001b84d69298ecb2e8ab57a45a2b602c78
MartinaPerozzi <perozzimartina@gmail.com>
2023-05-11 16:49:05 +0200

    + added UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User;
+        $user->name = "Pippo";
+        $user->email = "pippo@gmail.com";
+        $user->password = "password";
+        $user->save();
+    }
+}
