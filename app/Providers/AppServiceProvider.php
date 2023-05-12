79db27768dec1079e2c7c1525a37a2aa514acc09
UmbertoDV <biccoumby2002@hotmail.com>
2023-05-12 15:46:13 +0200

    fix paginator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
